864d6d6c334e57bbf148d85b0edd2aa680392435
sappiajeremias <jeremiassappia@gmail.com>
2022-04-18 17:16:25 -0300

    Comienzo de tp entrega 2

---
+++ Entrega2/Pasajero.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre, apellido, numero de documento y teléfono. El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero. También se desea guardar la información de la persona responsable de realizar el viaje, para ello cree una clase ResponsableV que registre el número de empleado, número de licencia, nombre y apellido. La clase Viaje debe hacer referencia al responsable de realizar el viaje. 
+Volver a implementar las operaciones que permiten modificar el nombre, apellido y teléfono de un pasajero. Luego implementar la operación que agrega los pasajeros al viaje, solicitando por consola la información de los mismos. Se debe verificar que el pasajero no este cargado mas de una vez en el viaje. De la misma forma cargue la información del responsable del viaje.*/
+
+
+class Pasajero {
+
+    //Atributos de la clase
+    private $nombre;
+    private $apellido;
+    private $dni;
+    private $telefono;
+
+    //Metodo constructor
+    public function __construct($pNombre, $pApellido, $pDni, $pTelefono){
+        $this->nombre = $pNombre;
+        $this->apellido = $pApellido;
+        $this->dni = $pDni;
+        $this->telefono = $pTelefono;
+    }
+
+    //Metodos observadores
+
+    /**
+     * Metodo que retorna el nombre
+     * @return string
+     */
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    /**
+     * Metodo que retorna el apellido
+     * @return string
+     */
+    public function getApellido (){
+        return $this->apellido;
+    }
+
+    /**
+     * Metodo que retorna el dni
+     * @return int
+     */
+    public function getDni(){
+        return $this->dni;
+    }
+
+    /**
+     * Metodo que retorna el telefono
+     * @return int
+     */
+    public function getTelefono(){
+        return $this->telefono;
+    }
+
+    //Metodos modificadores
+
+    /**
+     * Metodo que modifica el nombre
+     * @param string $pNombre
+     */
+    public function setNombre($pNombre){
+        $this->nombre = $pNombre;
+    }
+
+    /**
+     * Metodo que modifica el apellido
+     * @param string $pApellido
+     */
+    public function setApellido($pApellido){
+        $this->nombre = $pApellido;
+    }
+
+    /**
+     * Metodo que modifica el telefono
+     * @param int $pTelefono
+     */
+    public function setTelefono($pTelefono){
+        $this->telefono = $pTelefono;
+    }
+
+    //Metodo toString
+
+    public function __toString(){
+        echo "\nNombre y apellido: " . $this->getNombre() . " " . $this->getApellido() . "\nDNI: " . $this->getDni() . "\nTelefono: " . $this->telefono . ".\n";
+    }
+}
---
+++ Entrega2/ViajeFeliz.php
@@ -0,0 +1,91 @@
+<?php
+
+class ViajeFeliz {
+
+    //Atributos de la clase
+    private $codigo;
+    private $destino;
+    private $maxPasajeros;
+    private $pasajeros;
+
+    //Metodo constructor
+    public function __construct($pCod, $pDestino, $pMaxPasajeros, $aPasajeros){
+        $this->codigo = $pCod;
+        $this->destino = $pDestino;
+        $this->maxPasajeros = $pMaxPasajeros;
+        $this->pasajeros = $aPasajeros;
+    }
+
+    //Metodos observadores
+
+    /**
+     * Metodo que retorna el codigo del vuelo
+     * @return string
+     */
+    public function getCodigo(){
+        return $this->codigo;
+    }
+
+    /**
+     * Metodo que retorna el destino del vuelo
+     * @return string
+     */
+    public function getDestino(){
+        return $this->destino;
+    }
+
+    /**
+     * Metodo que retorna el maximo de pasajeros del avion
+     * @return int
+     */
+    public function getMaxPasajeros(){
+        return $this->maxPasajeros;
+    }
+
+    /**
+     * Metodo que retorna los pasajeros del vuelo
+     * @return string
+     */
+    public function getPasajeros(){
+        return $this->pasajeros;
+    }
+
+    //Metodos Modificadores
+    //El codigo no se modifica ya que es la clave del vuelo
+
+    /**
+     * Metodo que modifica el destino del vuelo
+     * @param string $pDestino
+     */
+    public function setDestino ($pDestino){
+        $this->destino = $pDestino;
+    }
+
+    /**
+     * Metodo que modifica el maximo de pasajeros del avion
+     * @param int $pMax
+     */
+    public function setMaxPasajeros ($pMax){
+        $this->maxPasajeros = $pMax;
+    }
+
+    /**
+     * Metodo que modifica un pasajero en un asiento del avion
+     * @param int $asientoPasajero
+     */
+    public function setPasajeros($aPasajeros){
+        $this->pasajeros = $aPasajeros;
+    }
+
+    //Metodo toString
+
+    public function __toString(){
+        $cadena = "";
+        for($i = 0; $i < count($this->pasajeros); $i++){
+            $cadena = $cadena . "Pasajero en el asiento " . $i+1 . ": \nNombre: " . $this->pasajeros[$i]["nombre"] . " " . $this->pasajeros[$i]["apellido"] . ".\nDNI: " . $this->pasajeros[$i]["documento"] . ".\n";
+        }
+        return "\nVuelo " . $this->getCodigo() . ".\nDestino: " . $this->getDestino() . "\nCantidad maxima de pasajeros: " . $this->getMaxPasajeros() . ".\nPasajeros:\n" . $cadena;
+    }
+}
+
+?>
